4a5875def89109169a7cf0fb37900de8089e74a1
DanielDeLaGarza <josedelagarza@u.boisestate.edu>
2016-08-03 15:04:12 -0700

    Moved logic from doogle controller to the Word model

--- app/controllers/doogle_controller.rb
+++ app/controllers/doogle_controller.rb
@@ -6,31 +6,26 @@ class DoogleController < ApplicationController
     input = params[:word_entry][:word]
     @word = Word.find_by(content: input)
 
-    #if word is already in database
     if(@word != nil)
-
-      @definitions = format_definitions(@word.definitions)
+      @definitions = @word.formated_definitions
       @validation_text = ""
-      #byebug
 
-    #if word is not already in database
     else
       @word = Word.new()
       @word.content = params[:word_entry][:word]
 
-      if(@word.valid?)
+      if @word.valid?
         @definitions = get_word_definitions_from_api(@word.content)
-        if (@definitions != nil && @definitions.length > 0 )
-          @word.save
-          save_definitions(@word, @definitions)
+
+        if @definitions != nil && @definitions.length > 0
+          @word.save_with_definitions(@definitions)
+          @validation_text = @word.get_validation_text
 
         else
           @validation_text = "The word #{@word.content} was not found in our dictionary. are you sure you spelled it right?"
         end
-
-      #ivalid word handling
       else
-        @validation_text = get_validation_text(@word.content)
+        @validation_text = @word.get_validation_text
         @definitions = []
       end
     end
@@ -42,44 +37,15 @@ class DoogleController < ApplicationController
 
   #_PRIVATE__METHODS____________________________________________________________________
   private
-    def get_validation_text(word)
-      if(word == nil || word == "")
-        @validation_text = "Please type a word to search."
-      else
-        if(word =~ /\d/)
-          @validation_text = "Words may not include digits!"
-        end
-        if(word =~ /\s/)
-          @validation_text = "#{@validation_text} Words may not include spaces!"
-        end
-        @validation_text = "#{@validation_text} Please only use letters and hyphens."
-      end
-      return @validation_text
-    end
-
     def get_word_definitions_from_api(word)
       conn = Faraday.new(:url => 'http://www.dictionaryapi.com/api/v1/references/collegiate/xml') do |faraday|
         faraday.request  :url_encoded             # form-encode POST params
         faraday.response :logger                  # log requests to STDOUT
         faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
       end
-
       response = conn.get word, { :key => 'cab72891-f003-43ef-a983-253666d45082' }
+
       doc  = Nokogiri::XML(response.body)
-      return doc.xpath("//dt").map {|d| d.text.gsub!(':', '')}
-    end
-
-    def format_definitions(definitions)
-      arr = []
-      definitions.each do |definition|
-        arr.push(definition.content)
-      end
-      return arr
-    end
-
-    def save_definitions(word, definitions)
-      definitions.each do |definition|
-        word.definitions.create(content: definition)
-      end
+      doc.xpath("//dt").map {|d| d.text.gsub!(':', '')}
     end
 end
--- app/models/word.rb
+++ app/models/word.rb
@@ -2,4 +2,36 @@ class Word < ApplicationRecord
   has_many :definitions, dependent: :destroy
   before_save {content.downcase!}
   validates :content, presence: true, allow_blank: false, format: { with: /\A[A-Za-z-]*\z/ }, uniqueness:  {case_sensitive: false}
+
+  def save_with_definitions(definitions)
+    self.save
+    definitions.each do |definition|
+      self.definitions.create(content: definition)
+    end
+  end
+
+  def formated_definitions
+    self.definitions.map {|d| d.content}
+  end
+
+  def get_validation_text
+    if !self.valid?
+      word = self.content
+      validation_text = ""
+      if word == nil || word == ""
+        validation_text = "Please type a word to search."
+      else
+        if word =~ /\d/
+          validation_text = "Words may not include digits!"
+        end
+        if word =~ /\s/
+          validation_text = "#{validation_text} Words may not include spaces!"
+        end
+        validation_text = "#{validation_text} Please only use letters and hyphens."
+      end
+      validation_text
+    else
+      ""
+    end
+  end
 end
